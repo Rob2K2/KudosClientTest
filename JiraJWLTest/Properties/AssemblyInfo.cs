cbf00aebf8557106967d86e1583306085d433b21
Rob2K2 <merino@live.com>
2019-10-20 19:03:45 -0400

    First Commit Se creó el proyecto de testing que prueba la aplicacion frontend de asignacion de KUDOS a usuarios registrados. Se crearon dos metodos: - LoginSuccessful: Prueba el correcto inicio de sesion en la aplicación, las credenciales estan hardcoded y son merino@live.com/12345, la prueba pasa si la respuesta que se muestra en la siguiente página es 'KUDOS'. - LoginUnsuccessful: Prueba el inicio de sesión incorrecto, la prueba pasa si la aplicación devuelve el mensaje 'Invalid login attempt.'.

---
+++ JiraJWLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JiraJWLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JiraJWLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8732bfa4-31ac-4778-8021-03fa88699809")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JiraJWLTest/TestClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace JiraJWLTest
+{
+    [TestClass]
+    public class TestClass
+    {
+        IWebDriver webDriver;
+
+        public TestClass()
+        {
+            webDriver = new ChromeDriver(@"C:\SeleniumWebDrivers");
+
+            webDriver.Navigate().GoToUrl("http://localhost:49988");
+        }
+
+        [TestMethod]
+        public void LoginSuccessful()
+        {
+            //Nvaigate to login page
+            IWebElement loginLink = webDriver.FindElement(By.Id("loginLink"));
+            loginLink.Click();
+
+            //Capture email input
+            IWebElement emailInput = webDriver.FindElement(By.Id("Email"));
+            emailInput.SendKeys("merino@live.com");
+
+
+            //Capture password input
+            IWebElement passwordInput = webDriver.FindElement(By.Id("Password"));
+            passwordInput.SendKeys("12345");
+
+
+            //capture login button
+            IWebElement loginButton = webDriver.FindElement(By.Id("loginButton"));
+            loginButton.Click();
+
+            //Capture login successful message
+            IWebElement successfulLoginMessage = webDriver.FindElement(By.XPath("//div[@class='jumbotron']/h1"));
+            string actualMessage = successfulLoginMessage.Text;
+
+            //Validate expected message is equal to login successful message
+            string expectedMessage = "KUDOS";
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod]
+        public void LoginUnsuccessful()
+        {
+            //Nvaigate to login page
+            IWebElement loginLink = webDriver.FindElement(By.Id("loginLink"));
+            loginLink.Click();
+
+            //Capture email input
+            IWebElement emailInput = webDriver.FindElement(By.Id("Email"));
+            emailInput.SendKeys("merino@live.com");
+
+
+            //Capture password input
+            IWebElement passwordInput = webDriver.FindElement(By.Id("Password"));
+            passwordInput.SendKeys("54321");
+
+
+            //capture login button
+            IWebElement loginButton = webDriver.FindElement(By.Id("loginButton"));
+            loginButton.Click();
+
+            //Capture login successful message
+            IWebElement errorLoginMessage = webDriver.FindElement(By.XPath("//div[@class='validation-summary-errors text-danger']/ul/li"));
+            string actualMessage = errorLoginMessage.Text;
+
+            //Validate expected message is equal to login successful message
+            string expectedMessage = "Invalid login attempt.";
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+    }
+}
