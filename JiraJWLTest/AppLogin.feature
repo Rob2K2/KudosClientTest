cd33d9945716249f0e429daf49b897264e4a4651
Rob2K2 <merino@live.com>
2019-10-21 02:02:09 -0400

    Commit with Specflow (Cucumber for .NET) Se agregaron las referencias para poder utilizar Specflow Se crearon los archivos: - AppLogin.feature: Contiene el codigo en Gherkin de los pasos para realizar la prueba de login en la aplicación. - AppLoginSteps.cs: Contiene el código para ejecutar los pasos del archivo AppLogin.feature.

---
+++ JiraJWLTest/AppLogin.feature
@@ -0,0 +1,19 @@
+﻿Feature: AppLogin
+	
+@mytag
+Scenario: Log in with correct credentials
+	Given I go to "http://localhost:49988"
+	And I click on Log in link
+	And I set username with "merino@live.com"
+	And I set password with "12345"
+	When I click on login button
+	Then I should see "KUDOS" message
+
+Scenario: Log in using incorrect credentials
+	Given I go to "http://localhost:49988"
+	And I click on Log in link
+	And I set username with "merino@live.com"
+	And I set password with "54231"
+	When I click on login button
+	Then I should see "Invalid login attempt." error label
+	
--- JiraJWLTest/AppLoginSteps.cs
+++ JiraJWLTest/AppLoginSteps.cs
@@ -1,79 +1,79 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
 
 namespace JiraJWLTest
 {
-    [TestClass]
-    public class TestClass
+    [Binding]
+    public class AppLoginSteps
     {
         IWebDriver webDriver;
 
-        public TestClass()
+        public AppLoginSteps()
         {
             webDriver = new ChromeDriver(@"C:\SeleniumWebDrivers");
-
-            webDriver.Navigate().GoToUrl("http://localhost:49988");
         }
 
-        [TestMethod]
-        public void LoginSuccessful()
+        [Given(@"I go to ""(.*)""")]
+        public void GivenIGoTo(string url)
+        {
+            //Go to URL
+            webDriver.Navigate().GoToUrl(url);
+        }
+        
+        [Given(@"I click on Log in link")]
+        public void GivenIClickOnLogInLink()
         {
             //Navigate to login page
             IWebElement loginLink = webDriver.FindElement(By.Id("loginLink"));
             loginLink.Click();
+        }
 
+        [Given(@"I set username with ""(.*)""")]
+        public void GivenISetUsernameWith(string email)
+        {
             //Capture email input
             IWebElement emailInput = webDriver.FindElement(By.Id("Email"));
-            emailInput.SendKeys("merino@live.com");
+            emailInput.SendKeys(email);
+        }
 
-
+        [Given(@"I set password with ""(.*)""")]
+        public void GivenISetPasswordWith(string password)
+        {
             //Capture password input
             IWebElement passwordInput = webDriver.FindElement(By.Id("Password"));
-            passwordInput.SendKeys("12345");
+            passwordInput.SendKeys(password);
+        }
 
-
+        [When(@"I click on login button")]
+        public void WhenIClickOnLoginButton()
+        {
             //capture login button
             IWebElement loginButton = webDriver.FindElement(By.Id("loginButton"));
             loginButton.Click();
+        }
 
+        [Then(@"I should see ""(.*)"" message")]
+        public void ThenIShouldSeeMessage(string expectedMessage)
+        {
             //Capture login successful message
             IWebElement successfulLoginMessage = webDriver.FindElement(By.XPath("//div[@class='jumbotron']/h1"));
             string actualMessage = successfulLoginMessage.Text;
 
             //Validate expected message is equal to login successful message
-            string expectedMessage = "KUDOS";
-
             Assert.AreEqual(expectedMessage, actualMessage);
         }
 
-        [TestMethod]
-        public void LoginUnsuccessful()
+        [Then(@"I should see ""(.*)"" error label")]
+        public void ThenIShouldSeeErrorLabel(string p0)
         {
-            //Nvaigate to login page
-            IWebElement loginLink = webDriver.FindElement(By.Id("loginLink"));
-            loginLink.Click();
-
-            //Capture email input
-            IWebElement emailInput = webDriver.FindElement(By.Id("Email"));
-            emailInput.SendKeys("merino@live.com");
-
-
-            //Capture password input
-            IWebElement passwordInput = webDriver.FindElement(By.Id("Password"));
-            passwordInput.SendKeys("54321");
-
-
-            //capture login button
-            IWebElement loginButton = webDriver.FindElement(By.Id("loginButton"));
-            loginButton.Click();
-
-            //Capture login unsuccessful message
+            //Capture error message
             IWebElement errorLoginMessage = webDriver.FindElement(By.XPath("//div[@class='validation-summary-errors text-danger']/ul/li"));
             string actualMessage = errorLoginMessage.Text;
 
-            //Validate expected message is equal to login successful message
+            //Validate expected message is equal to error message
             string expectedMessage = "Invalid login attempt.";
 
             Assert.AreEqual(expectedMessage, actualMessage);
